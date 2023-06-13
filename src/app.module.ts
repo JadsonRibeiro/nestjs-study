6ae2120ec94bdce9024840de35c3e689cbfaa9df
Jadson (Mac) <jdsntj@gmail.com>
2023-06-13 14:49:08 -0300

    create middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/Logger';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'users', method: RequestMethod.GET })
+      .forRoutes(AppController);
+  }
+}
